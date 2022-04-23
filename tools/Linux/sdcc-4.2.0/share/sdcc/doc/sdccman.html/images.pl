9d6d67b7b602b6254bd77ba0aa87348d218ebec6
Ryan Hartlage <ryanplusplus@gmail.com>
2022-04-23 07:09:54 -0400

    sdcc-4.2.0

---
+++ tools/Linux/sdcc-4.2.0/share/sdcc/doc/sdccman.html/images.pl
@@ -0,0 +1,43 @@
+# LaTeX2HTML 2020.2 (Released July 1, 2020)
+# Associate images original text with physical files.
+
+
+$key = q/Omega;MSF=1;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="" SRC="|."$dir".q|img9.png"
+ ALT="$\Omega$">|; 
+
+$key = q/^{textrm{2}};MSF=1;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="" SRC="|."$dir".q|img3.png"
+ ALT="$^{\textrm{2}}$">|; 
+
+$key = q/^{text{TM}};MSF=1;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="" SRC="|."$dir".q|img8.png"
+ ALT="$^{\text{TM}}$">|; 
+
+$key = q/includegraphics{MCS51_named};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="" SRC="|."$dir".q|img2.png"
+ ALT="\includegraphics{MCS51_named}">|; 
+
+$key = q/includegraphics{r3ka-arguments};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="" SRC="|."$dir".q|img5.png"
+ ALT="\includegraphics{r3ka-arguments}">|; 
+
+$key = q/includegraphics{sm83-arguments};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="" SRC="|."$dir".q|img6.png"
+ ALT="\includegraphics{sm83-arguments}">|; 
+
+$key = q/includegraphics{stm8-arguments};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="" SRC="|."$dir".q|img7.png"
+ ALT="\includegraphics{stm8-arguments}">|; 
+
+$key = q/includegraphics{z80-arguments};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="" SRC="|."$dir".q|img4.png"
+ ALT="\includegraphics{z80-arguments}">|; 
+
+$key = q/{center}vbox{input{sdcc_version}}{center};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="" SRC="|."$dir".q|img1.png"
+ ALT="\begin{center}\vbox{\input{sdcc_version}
+}\end{center}">|; 
+
+1;
+
---
+++ tools/Linux/sdcc-4.2.0/share/sdcc/doc/sdccman.html/internals.pl
@@ -0,0 +1,226 @@
+# LaTeX2HTML 2020.2 (Released July 1, 2020)
+# Associate internals original text with physical files.
+
+
+$key = q/OMF_file/;
+$ref_files{$key} = "$dir".q|node47.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:Debugging-with-SDCDB/;
+$ref_files{$key} = "$dir".q|node165.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ite:callee_saves-function1_00005B_function2_00005B_function3..._00005D_00005D--/;
+$ref_files{$key} = "$dir".q|node109.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ite:less_pedantic/;
+$ref_files{$key} = "$dir".q|node109.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ite:pedantic_parse_number/;
+$ref_files{$key} = "$dir".q|node109.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lyx:--callee-saves-function1_00005B_function2_00005D_00005B_function3_00005D.../;
+$ref_files{$key} = "$dir".q|node56.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lyx:--less-pedantic/;
+$ref_files{$key} = "$dir".q|node56.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lyx:--use-non-free/;
+$ref_files{$key} = "$dir".q|node56.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lyx:-Wl_option/;
+$ref_files{$key} = "$dir".q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lyx:-codeseg/;
+$ref_files{$key} = "$dir".q|node56.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lyx:-pedantic-parse-number/;
+$ref_files{$key} = "$dir".q|node54.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lyx:more-pedantic-SPLINT/;
+$ref_files{$key} = "$dir".q|node56.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:Command-Line-Options/;
+$ref_files{$key} = "$dir".q|node52.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:Compatibility-with-previous/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:Inline-Assembler-Code/;
+$ref_files{$key} = "$dir".q|node93.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:Parameters-and-Local-Variables/;
+$ref_files{$key} = "$dir".q|node78.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:Porting-code-to-other-compilers/;
+$ref_files{$key} = "$dir".q|node175.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:Pragmas/;
+$ref_files{$key} = "$dir".q|node109.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:Quality-control/;
+$ref_files{$key} = "$dir".q|node191.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:Related-open-source-tools/;
+$ref_files{$key} = "$dir".q|node179.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:Testing-the-SDCC/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:A-Step-by_Assembler_Introduction/;
+$ref_files{$key} = "$dir".q|node95.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Bankswitching/;
+$ref_files{$key} = "$dir".q|node115.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Building-SDCC-on-Linux/;
+$ref_files{$key} = "$dir".q|node15.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Common-interrupt-pitfall-non-atomic/;
+$ref_files{$key} = "$dir".q|node81.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Common-interrupt-pitfall-non-reentrant/;
+$ref_files{$key} = "$dir".q|node81.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Common-interrupt-pitfall-stack-overflow/;
+$ref_files{$key} = "$dir".q|node81.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Common-interrupt-pitfall-volatile/;
+$ref_files{$key} = "$dir".q|node81.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:External-Stack/;
+$ref_files{$key} = "$dir".q|node106.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Functions-using-private-banks/;
+$ref_files{$key} = "$dir".q|node92.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Install-Trouble-shooting/;
+$ref_files{$key} = "$dir".q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Install-paths/;
+$ref_files{$key} = "$dir".q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Intermediate-Dump-Options/;
+$ref_files{$key} = "$dir".q|node64.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Interrupt-Service-Routines/;
+$ref_files{$key} = "$dir".q|node80.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Loop-Optimizations/;
+$ref_files{$key} = "$dir".q|node199.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:MCS51-Startup-Code/;
+$ref_files{$key} = "$dir".q|node116.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:MCS51-variants/;
+$ref_files{$key} = "$dir".q|node112.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Naked-Functions/;
+$ref_files{$key} = "$dir".q|node96.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Naming-of-special/;
+$ref_files{$key} = "$dir".q|node141.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Overlaying/;
+$ref_files{$key} = "$dir".q|node79.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:PIC16_Header-Files/;
+$ref_files{$key} = "$dir".q|node151.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:PIC16_Header-Files-and-Libraries/;
+$ref_files{$key} = "$dir".q|node150.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:PIC16_Pragmas/;
+$ref_files{$key} = "$dir".q|node149.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Peephole-Optimizer/;
+$ref_files{$key} = "$dir".q|node211.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Postprocessing-the-Intel/;
+$ref_files{$key} = "$dir".q|node48.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Register-Allocation/;
+$ref_files{$key} = "$dir".q|node210.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Requesting-Features/;
+$ref_files{$key} = "$dir".q|node185.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Search-Paths/;
+$ref_files{$key} = "$dir".q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Standard-Compliance/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:The-anatomy-of/;
+$ref_files{$key} = "$dir".q|node215.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Using-sdar-to/;
+$ref_files{$key} = "$dir".q|node51.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Windows-Install/;
+$ref_files{$key} = "$dir".q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:_switch_-Statements/;
+$ref_files{$key} = "$dir".q|node202.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:pic16Libraries/;
+$ref_files{$key} = "$dir".q|node152.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/type_promotion/;
+$ref_files{$key} = "$dir".q|node174.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
